98721776ddc6819fd2eee10f88c749ab59a062ec
jpeadebo <andydebolt@gmail.com>
2022-03-25 19:13:11 -0700

    matrix should be actually working as intended now

--- matrix.py
+++ matrix.py
@@ -1,79 +1,105 @@
+def transpose(list):
+    matrixN = [[0 for x in range(1)] for y in range(len(list))]
+    for r in range(len(list)):
+        for c in range(1):
+            matrixN[r][c] = list[r]
+    return matrixN
+
 class Matrix:
 
-    def __init__(self, list2D):
-        self.setMatrix(list2D)
-
-    def setMatrix(self, newMatrix):
-        self.matrix = newMatrix
-        self.colSize = len(newMatrix[0])
-        self.rowSize = len(newMatrix)
-
-    def vector(self, newVector):
-        matrixV = []
-        for i in newVector:
-            vector = [1]
-            vector[0] = i
-            matrixV.append(vector)
-        self.matrix = matrixV
-        self.colSize = 1
-        self.rowSize = len(newVector)
+    def __init__(self, mat):
+        # [row][col]
+        self.matrix = mat
+        if self.getRowSize() == 1:
+            print("transposing matrix since its a unrotated vector", mat)
+            self.transpose()
 
     def printMatrix(self):
-        for i in self.matrix:
-            print(i)
+        for r in self.matrix:
+            print(r)
 
-    def setElement(self, pos, value):
-        self.matrix[pos[0]][pos[1]] = value
-
+    # pos = [row, col]
     def at(self, pos):
         return self.matrix[pos[0]][pos[1]]
 
-    def getRow(self, rowPos):
-        return self.matrix[rowPos]
+    def setMatrix(self, mat):
+        self.matrix = mat
 
-    def getCol(self, colPos):
-        col = []
-        for i in self.matrix:
-            col.append(i[colPos])
-        return col
-
-    def getColSize(self):
-        return self.colSize
+    def getRow(self, row):
+        return self.matrix[row]
 
     def getRowSize(self):
-        return self.rowSize
+        return len(self.matrix)
+
+    def getCol(self, col):
+        list = []
+        for r in self.matrix:
+            if len(r) > col:
+                list.append(r[col])
+        return transpose(list)
+
+    def getColSize(self):
+        return len(self.getRow(0))
 
     def addElement(self, pos, value):
         self.matrix[pos[0]][pos[1]] += value
 
     def add(self, matrix2):
-        if self.colSize == matrix2.getColSize() and self.rowSize == matrix2.getRowSize():
+        if self.getColSize() == matrix2.getColSize() and self.getRowSize() == matrix2.getRowSize():
             for r in range(len(self.matrix)):
                 for c in range(len(self.matrix[r])):
                     self.addElement([r,c], matrix2.at([r,c]))
 
     def subtract(self, matrix2):
-        if self.colSize == matrix2.getColSize() and self.rowSize == matrix2.getRowSize():
+        if self.getColSize() == matrix2.getColSize() and self.getRowSize() == matrix2.getRowSize():
             for r in range(len(self.matrix)):
                 for c in range(len(self.matrix[r])):
                     self.addElement([r, c], -matrix2.at([r, c]))
 
+    def vectorVectorMult(self, vector2):
+        if isinstance(vector2, list):
+            vector2 = Matrix(vector2)
+        if self.getRowSize() == vector2.getRowSize() and self.getColSize() == 1 and vector2.getColSize() == 1:
+            print("element wise vector vector mult")
+            sum = 0
+            for r in range(len(self.matrix)):
+                sum += self.at([r, 0]) * vector2.at([r,0])
+            return sum
+        elif self.getRowSize() == vector2.getColSize() and self.getColSize() == 1 and vector2.getRowSize() == 1:
+            print("vector * vector transpose")
+            mat = [[(self.at([r, 0]) * vector2.at([0,c])) for c in range(self.getRowSize())] for r in range(vector2.getColSize())]
+            return mat
+        else:
+            print("bruh enter the right shit into vector vector mult")
+
+    def dotProduct(self, matrix2):
+        # take a layer
+        dVector = []
+        if matrix2.getColSize() > 1:
+            for r in matrix2.matrix:
+                dVector.append(self.vectorVectorMult(transpose(r)))
+        elif self.getColSize() > 1:
+            for r in self.matrix:
+                dVector.append(matrix2.vectorVectorMult(transpose(r)))
+        return dVector
+
     def multiply(self, matrix2):
-        if self.colSize == matrix2.getRowSize():
+        if self.getColSize() == matrix2.getRowSize():
+            print("here")
             colSizeN = matrix2.getColSize()
-            rowSizeN = self.rowSize
+            rowSizeN = self.getRowSize()
             matrix = [[0 for x in range(colSizeN)] for y in range(rowSizeN)]
             for r in range(rowSizeN):
                 for c in range(colSizeN):
                     # add together the multiplications of the col and row
                     sum = 0
                     for k in range(len(matrix2.getCol(c))):
-                        sum += matrix2.getCol(c)[k] * self.getRow(r)[k]
+                        sum += matrix2.getCol(c)[k][0] * self.getRow(r)[k]
                     matrix[r][c] = sum
             self.setMatrix(matrix)
 
-        elif self.rowSize == matrix2.getColSize():
-            colSizeN = self.colSize
+        elif self.getRowSize() == matrix2.getColSize():
+            colSizeN = self.getColSize()
             rowSizeN = matrix2.getRowSize()
             matrix = [[0 for x in range(colSizeN)] for y in range(rowSizeN)]
             for r in range(rowSizeN):
@@ -81,17 +107,25 @@ class Matrix:
                     # add together the multiplications of the col and row
                     sum = 0
                     for k in range(len(matrix2.getRow(r))):
-                        sum += matrix2.getRow(r)[k] * self.getCol(c)[k]
+                        sum += matrix2.getRow(r)[k] * self.getCol(c)[k][0]
+                    print(sum)
                     matrix[r][c] = sum
             self.setMatrix(matrix)
-        elif matrix2.getColSize() == 1 and matrix2.getRowSize() == 1:
-            for r in range(len(self.matrix)):
-                for c in range(len(self.matrix[r])):
-                    self.setElement([r, c], self.at([r, c]) * matrix2.at([r, c]))
 
+        else:
+            print("bruh put the right shit into multiply")
     def transpose(self):
-        matrixN = [[0 for x in range(self.rowSize)] for y in range(self.colSize)]
+        matrixN = [[0 for x in range(self.getRowSize())] for y in range(self.getColSize())]
         for r in range(len(self.matrix)):
             for c in range(len(self.matrix[r])):
                 matrixN[c][r] = self.at([r, c])
         self.setMatrix(matrixN)
+
+
+# [row] [col]
+m = [[2,3,2,2], [1,1,1,1]]
+v = [[3,3,3,9],[1,2,7,2]]
+matrix = Matrix(m)
+vector = Matrix(v)
+matrix.add(vector)
+matrix.printMatrix()
--- network.py
+++ network.py
@@ -1,6 +1,9 @@
 import math
 import random
 
+from matrix import Matrix
+
+
 def sigmoidFunction(z):
     if z < 100:
         z = 100
@@ -14,179 +17,50 @@ outputScale = 1
 learningRate = .1
 
 
+# rows represent layers and cols represent which prev node its connected to
+def makeWeights(framework):
+    prevLayerLen = framework[0]
+    # for each layer in the framework excluding the input layer
+    layerMatrix = []
+    for L in framework[1:]:
+        matrix = Matrix()
+        matrix.setMatrix([[random.uniform(-1, 1) for r in range(prevLayerLen)] for c in range(L)])
+        layerMatrix.append(matrix)
+        prevLayerLen = L
+    return layerMatrix
+
+
+# row represents layer col represents node
+def makeValueMatrix(framework):
+    valueMatrix = []
+    for L in framework:
+        vector = [0 for r in range(L)]
+        valueMatrix.append(vector)
+    vMatrix = Matrix()
+    vMatrix.setMatrix(valueMatrix)
+    return vMatrix
+
+
 class Network:
 
-    def __init__(self, frameWork):
-        self.framework = frameWork
-        self.numLayers = len(self.framework)
-        self.layers = []
-        self.createFramework()
-        self.setWeightsRandom()
+    def __init__(self, framework):
+        self.layerMatrix = makeWeights(framework)
+        self.valueMatrix = makeValueMatrix(framework)
+        self.feedForward()
+        self.valueMatrix.printMatrix()
 
-    def outputLayer(self):
-        return self.layers[self.numLayers - 1]
-
-    def displayOutput(self):
-        return self.outputLayer() * outputScale
-
-    def printWeights(self):
-        for i in self.layers:
-            for j in i:
-                print(j.getWeights())
-            print("\n")
-
-    def printValues(self):
-        for i in self.layers:
-            for j in i:
-                print(j.getValue())
-            print("\n")
-
-    def printOutputs(self):
-        for i in self.outputLayer():
-            print(i.getValue())
-        print("\n")
-
-    def printError(self):
-        for i in self.outputLayer():
-            print(i.getError())
-
-    def createFramework(self):
-        for i in self.framework:
-            layer = []
-            for j in range(0, i):
-                neuron = Neuron()
-                layer.append(neuron)
-            self.layers.append(layer)
-
-    def setInputs(self, inputs):
-        for i in range(len(inputs)):
-            self.layers[0][i].setValue(inputs[i])
-
-    def setWeightsRandom(self):
-        for i in range(len(self.layers)):
-            for j in range(len(self.layers[i])):
-                randomList = []
-                for k in range(len(self.layers[(i if i == 0 else i - 1)])):
-                    randomList.append(0 if i == 0 else random.uniform(-1, 1))
-                self.layers[i][j].setWeights(randomList)
-
-    def getLayerValues(self, level):
-        layerValues = []
-        for i in range(len(self.layers[level])):
-            layerValues.append(self.layers[level][i].getValue())
-        return layerValues
-
-    def calcOutputs(self):
-        for i in range(len(self.layers)):
-            if i != 0:
-                prevLayerValues = self.getLayerValues(i - 1)
-                for j in range(len(self.layers[i])):
-                    self.layers[i][j].calcValue(prevLayerValues)
-
-    # end of network setup
-    # start of backprop
-    def calcError(self, target):
-        prevLayer = self.outputLayer()
-        for i in prevLayer:
-            i.setError(target - i.getValue())
-        # layers[1:-1] means remove input and output layers from being changed
-        for j in reversed(self.layers[1:-1]):
-            for i in range(len(j)):
-                nodeError = 0
-                for l in prevLayer:
-                    strength = l.getWeights()[i]
-                    nodeError += strength * l.getError()
-                j[i].setError(nodeError)
-            prevLayer = j
-        return self.layers[self.numLayers - 1][0].getError()
-
-    def calcDeltaWeights(self):
-        # element wise multiplication
-        prevLayer = self.layers[0]
-        dArrayWeights = []
-        for j in self.layers[1:]:
-            dLayerWeight = []
-            for i in range(len(j)):
-                sigmoidInverse = j[i].getValue() * (1 - j[i].getValue())
-                dWeight = learningRate * j[i].getError() * sigmoidInverse
-                dLayerWeight.append(dWeight)
-            layerWeightCol = []
-            for c in dLayerWeight:
-                layerWeightRow = []
-                for r in prevLayer:
-                    layerWeightRow.append(c * r.getValue())
-                layerWeightCol.append(layerWeightRow)
-            dArrayWeights.append(layerWeightCol)
-            prevLayer = j
-        return dArrayWeights
-
-    def applyDeltaWeights(self):
-        dArrayWeights = self.calcDeltaWeights()
-        for i in range(len(dArrayWeights)):
-            for j in range(len(dArrayWeights[i])):
-                self.layers[i + 1][j].updateWeights(dArrayWeights[i][j])
-
-    def train(self, inputs):
-        for input in inputs:
-            if len(input) != 0:
-                self.setInputs(input[:-1])
-                target = input[len(input)-1]
-                self.calcOutputs()
-                print(target, self.outputLayer()[0].getValue())
-                self.calcError(target)
-                self.applyDeltaWeights()
-
-    def test(self, inputs):
-        for input in inputs:
-            if len(input) != 0:
-                self.setInputs(input[:-1])
-                self.calcOutputs()
-                self.printError()
+    def feedForward(self):
+        for layer in range(len(self.layerMatrix)):
+            vector = Matrix()
+            valueVector = self.valueMatrix.getRow(layer)
+            vector.vector(valueVector)
+            vector.multiply(self.layerMatrix[layer])
+            self.valueMatrix.setRow(layer + 1, vector.getRow(0))
 
 
-class Neuron:
+inputLength = 5
+hiddenLayer1Length = 4
+outputLength = 2
 
-    def __init__(self):
-        # weights is the list of connecting values between the current neuron and the previous layer
-        self.weights = []
-        self.value = 0
-        self.bias = 0
-        self.error = 0
-
-    def getWeights(self):
-        return self.weights
-
-    def getValue(self):
-        return self.value
-
-    def getBias(self):
-        return self.bias
-
-    def getError(self):
-        return self.error
-
-    def setWeights(self, w):
-        self.weights = w
-
-    def setValue(self, v):
-        self.value = v
-
-    def setBias(self, b):
-        self.bias = b
-
-    def setError(self, e):
-        self.error = e
-
-    def updateWeights(self, dWeight):
-        updatedWeights = []
-        currentWeights = self.getWeights()
-        for i in range(len(currentWeights)):
-            updatedWeights.append(currentWeights[i] + dWeight[i])
-        self.setWeights(updatedWeights)
-
-    def calcValue(self, prevLayerValues):
-        summation = 0.0
-        for i in range(len(prevLayerValues)):
-            summation += self.getWeights()[i] * prevLayerValues[i]
-        z = summation + self.getBias()
-        self.setValue(sigmoidFunction(z))
+framework = [inputLength, hiddenLayer1Length, outputLength]
+net = Network(framework)
